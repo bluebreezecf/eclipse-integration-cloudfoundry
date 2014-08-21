acc049e05cf3904d2d44a3a509915e9eac484438
bluebreezecf <shijianwei2020@163.com>
2014-08-21 21:11:37 +0000

    Fix the bug that dragging and dropping a project which has no relation with a cloud application to the applicationViewer will cause the existing cloud application with the same name to be deleted.

--- org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/editor/ApplicationMasterPart.java
+++ org.cloudfoundry.ide.eclipse.server.ui/src/org/cloudfoundry/ide/eclipse/server/ui/internal/editor/ApplicationMasterPart.java
@@ -43,6 +43,7 @@ import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.util.LocalSelectionTransfer;
 import org.eclipse.jface.viewers.DecorationOverlayIcon;
@@ -54,6 +55,7 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DragSourceAdapter;
@@ -64,6 +66,7 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.SectionPart;
@@ -206,6 +209,23 @@ public class ApplicationMasterPart extends SectionPart {
 									.getOriginal().loadAdapter(CloudFoundryServer.class, monitor);
 
 							if (cloudServer != null) {
+                                                                // First of all, should make sure there is no CloundApplicationModule 
+								// with the same name of the selected project, otherwise it will cause
+								// the CloundApplicationModule with the same name and its related remote
+                                                                // application in CF to be deleted.
+								if (cloudServer.getBehaviour().existCloudApplicationModule(moduleName)) {
+									Display.getDefault().asyncExec(new Runnable() {
+
+										public void run() {
+											MessageDialog.openWarning(editorPage.getSite().getShell(), "Failed to deploy application", 
+											    NLS.bind("Can't deploy the project {0} because a cloud application with the same name already exits. {1}", moduleName,
+											        " Please either rename the project or delete the cloud application, and then try again."));
+										}
+										
+									});
+									return Status.CANCEL_STATUS;
+								}
+
 								// Make sure parent performs the drop first to
 								// create the IModule. Unsupported modules will
 								// not proceed result in IModule creation
